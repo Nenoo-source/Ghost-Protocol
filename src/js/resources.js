eb63e2b8e2600cedeac16cd3f864d9d22816c051
hoogendoornmax-code <hoogendoornmax@gmail.com>
2026-06-16 14:16:16 +0200

    x

--- src/js/resources.js
+++ src/js/resources.js
@@ -3,13 +3,8 @@ import { ImageSource, Sound, Resource, Loader } from 'excalibur'
 // voeg hier jouw eigen resources toe
 const Resources = {
     Background: new ImageSource('images/background.png'),
-<<<<<<< HEAD
-    player1: new ImageSource('images/fish.png'),
-    tv: new ImageSource('images/tvenemy.png')
-=======
     player1: new ImageSource('images/defensePlayer.png'),
-    tv: new ImageSource('images/mine.png')
->>>>>>> f80c92b9fc68213970defd435855888b9203bc55
+    tv: new ImageSource('images/tvenemy.png')
 }
 
 
