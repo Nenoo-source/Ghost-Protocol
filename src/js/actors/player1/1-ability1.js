be70bf51436f3918f666410342549185a1671654
Kay Bronder <bronderkay@gmail.com>
2026-06-18 12:49:56 +0200

    ability

---
+++ src/js/actors/player1/1-ability1.js
@@ -0,0 +1,6 @@
+import { Actor, Color, FadeInOut, Font, FontUnit, Keys, Label, Scene, Vector, DisplayMode, CollisionType, DegreeOfFreedom } from "excalibur"
+
+export class ThreatScanner extends Actor {
+
+
+}
