db4ee12482382038e7b6745ce73296f12265e2c5
IbsenE10 <23047364@student.hhs.nl>
2026-06-18 14:00:37 +0200

    ability 1 van player 2 is gefixed (superjump), hij gaat te hoog

--- src/js/actors/player2/2-ability1.js
+++ src/js/actors/player2/2-ability1.js
@@ -3,8 +3,13 @@ import { Keys, Vector } from "excalibur"
 // Ability 1: Super Jump (key 7)
 // Veel sterkere sprong dan de normale jump, alleen vanaf de grond, met cooldown.
 
-const SUPER_JUMP_FORCE = 900       // sterkte van de super jump (normale jump = 350)
-const SUPER_JUMP_COOLDOWN = 3000   // cooldown in ms
+// LET OP: de normale jump in playerBase.js gebruikt "-350 * delta" als impulse,
+// waarbij delta de frametijd in ms is (~16.67 bij 60fps). Dat is dus geen vaste
+// kracht maar een kracht die met de frametijd meeschaalt. De super jump moet
+// dezelfde "* delta" opzet gebruiken, anders is hij (per ongeluk) veel zwakker
+// dan de normale jump in plaats van sterker.
+const SUPER_JUMP_MULTIPLIER = 350 * 1.5  // 2.5x sterker dan de normale jump (normaal = 350)
+const SUPER_JUMP_COOLDOWN = 3000         // cooldown in ms
 
 export function setupSuperJump(player) {
     player.superJumpCooldownRemaining = 0
@@ -20,11 +25,11 @@ export function updateSuperJump(player, engine, delta) {
     }
 
     if (engine.input.keyboard.wasPressed(Keys.Digit7) || engine.input.keyboard.wasPressed(Keys.Num7)) {
-        tryActivateSuperJump(player)
+        tryActivateSuperJump(player, delta)
     }
 }
 
-function tryActivateSuperJump(player) {
+function tryActivateSuperJump(player, delta) {
     if (player.superJumpCooldownRemaining > 0) {
         // nog niet beschikbaar
         return false
@@ -35,7 +40,8 @@ function tryActivateSuperJump(player) {
         return false
     }
 
-    player.body.applyLinearImpulse(new Vector(0, -SUPER_JUMP_FORCE))
+    // zelfde patroon als de normale jump in playerBase.js: kracht * delta
+    player.body.applyLinearImpulse(new Vector(0, -SUPER_JUMP_MULTIPLIER * delta))
     player.superJumpCooldownRemaining = SUPER_JUMP_COOLDOWN
 
     return true
