5433a1dffab68b156177dd1a84516c633f12fc6e
Kay Bronder <bronderkay@gmail.com>
2026-06-18 13:32:37 +0200

    resources

--- src/js/actors/player1/1-ability1.js
+++ src/js/actors/player1/1-ability1.js
@@ -1,8 +1,19 @@
 import { Actor, Color, FadeInOut, Font, FontUnit, Keys, Label, Scene, Vector, DisplayMode, CollisionType, DegreeOfFreedom } from "excalibur"
+import { Resources } from "../../resources"
 
 export class ThreatScanner extends Actor {
-    constructor() {
-
+    constructor(x, y) {
+        super({
+            width: Resources.Projectile.width,
+            height: Resources.Projectile.height
+        })
+        this.posX = x
+        this.posY = y
     }
 
+    onInitialize(engine) {
+        this.scale(1, 1)
+        this.graphics.use(Resources.Projectile.toSprite())
+        this.pos = new Vector(this.posX, this.posY)
+    }
 }
--- src/js/actors/player1/player1.js
+++ src/js/actors/player1/player1.js
@@ -1,7 +1,8 @@
-import { Actor, Color, FadeInOut, Font, FontUnit, Keys, Label, Scene, Vector, DisplayMode, CollisionType, DegreeOfFreedom } from "excalibur"
+import { Actor, Color, FadeInOut, Font, FontUnit, Keys, Label, Scene, Vector, DisplayMode, CollisionType, DegreeOfFreedom, Delta } from "excalibur"
 import { Resources } from "../../resources"
 import { Ground } from '../ground.js'
 import { Player } from '../playerBase.js'
+import { ThreatScanner } from './1-ability1.js'
 
 export class Player1 extends Player {
 
@@ -19,5 +20,16 @@ export class Player1 extends Player {
         this.scale = new Vector(0.15, 0.15)
         this.graphics.use(Resources.player1.toSprite())
         this.pos = new Vector(100, 540)
+    } 
+
+    onPreUpdate(engine, delta) {
+        if (engine.input.keyboard.wasPressed(Keys.1)) {
+            
+        }
+    }
+
+    attackAbility() {
+        let shot = new ThreatScanner(this.pos.x, this.pos.y)
+        this.scene.add(shot)
     }
 }
