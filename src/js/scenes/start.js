ca35c4f46c3cb8c3d575872945dd84681480aaf2
hoogendoornmax-code <hoogendoornmax@gmail.com>
2026-06-18 12:46:55 +0200

    start scherm

--- src/js/scenes/start.js
+++ src/js/scenes/start.js
@@ -1,59 +1,49 @@
-import { Actor, Color, FadeInOut, Font, FontUnit, Keys, Label, Scene, Vector } from "excalibur"
+import { Actor, Color, FadeInOut, Font, FontUnit, Keys, Label, Scene, Vector, Text } from "excalibur"
 import { Resources } from "../resources.js"
 
 export class StartScene extends Scene {
     onInitialize(engine) {
         const background = new Actor({
-            // Background position
             x: engine.halfDrawWidth,
             y: engine.halfDrawHeight,
             width: engine.drawWidth,
             height: engine.drawHeight,
             z: -1,
         })
-        background.graphics.use(Resources.Background.toSprite())
+        background.graphics.use(Resources.start.toSprite())
         this.add(background)
         background.scale = new Vector(0.85, 0.8)
 
-
-        //title
-        const title = new Label({
-            text: "Ghost Protocol",
-            pos: new Vector(engine.halfDrawWidth, engine.halfDrawHeight - 200),
-            font: new Font({
-                size: 64,
-                unit: FontUnit.Px,
-                color: Color.fromHex('#FFF8E1'),
-                strokeColor: Color.fromHex('#ff0000'),
-                lineWidth: 3,
-            })
+        // title
+        const title = new Actor({
+            pos: new Vector(engine.halfDrawWidth, engine.halfDrawHeight + 40),
+            anchor: new Vector(0.5, 0.8)
         })
-        title.anchor = new Vector(0.5, 0.5)
+
+        title.graphics.use(new Text({
+            text:
+                "In today's connected world, the risks of getting hacked and\n" +
+                "cyber attacks are higher than ever.\n\n" +
+                "\"Ghost Protocol\" will immerse you in a world of cyber\n" +
+                "espionage where your mission is to outsmart the hackers\n" +
+                "and keep your personal data safe.\n\n" +
+                "Through gameplay you'll learn the importance of making sure\n" +
+                "your own data stays out of the wrong hands.",
+            font: new Font({
+                family: "Orbitron",
+                size: 24,
+                unit: FontUnit.Px,
+                color: Color.fromHex('#E0E0E0'),
+                strokeColor: Color.fromHex("#a8e1fb"),
+            })
+        }))
+
         this.add(title)
-
-        //instruction
-        const instruction = new Label({
-            text: "Press space to start",
-            pos: new Vector(engine.halfDrawWidth, engine.halfDrawHeight),
-            font: new Font({
-                size: 64,
-                unit: FontUnit.Px,
-                color: Color.fromHex('#FFF8E1'),
-                strokeColor: Color.fromHex('#ff0000'),
-                lineWidth: 3,
-            })
-        })
-        instruction.anchor = new Vector(0.5, 0.5)
-        this.add(instruction)
-
     }
 
-
-    // Go to game start
     onPreUpdate(engine) {
         if (engine.input.keyboard.wasPressed(Keys.Space)) {
             engine.goToScene("game", {
-                // transition
                 sourceOut: new FadeInOut({ duration: 600, direction: 'out' }),
                 destinationIn: new FadeInOut({ duration: 600, direction: 'in' })
             })
