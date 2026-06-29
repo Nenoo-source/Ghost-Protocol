15219ce32d2a12074a89848e853c66b7be80eea2
IbsenE10 <23047364@student.hhs.nl>
2026-06-29 11:01:21 +0200

    blocking abilty aangepast , tap instaed of holding.

--- src/js/actors/player2/2-ability3.js
+++ src/js/actors/player2/2-ability3.js
@@ -3,67 +3,33 @@ import { Resources } from "../../resources.js"
 
 // Ability 3: Block (key 9)
 
-const BLOCK_TYPES_COUNT = 4
+const BLOCK_DURATION = 2000 // ms
 
 export function setupBlock(player) {
     player.blockType = 1
     player.blocking = false
-    player.pendingBlockTap = false
-    player.blockHoldTimer = 0
+    player.blockTimer = 0
 }
 
 export function updateBlock(player, engine, delta) {
-    const HOLD_THRESHOLD = 120
-
-    const blockKeyHeld =
-        engine.input.keyboard.isHeld(Keys.Digit9) ||
-        engine.input.keyboard.isHeld(Keys.Num9)
-
     const blockKeyPressed =
         engine.input.keyboard.wasPressed(Keys.Digit9) ||
         engine.input.keyboard.wasPressed(Keys.Num9)
 
-    const blockKeyReleased =
-        engine.input.keyboard.wasReleased(Keys.Digit9) ||
-        engine.input.keyboard.wasReleased(Keys.Num9)
-
     if (blockKeyPressed && !player.blocking) {
-        player.pendingBlockTap = true
-        player.blockHoldTimer = 0
         player.blocking = true
-
-        // start blocking right away on tap
+        player.blockTimer = BLOCK_DURATION
         player.graphics.use(Resources.defensiveState.toSprite())
     }
 
-    if (blockKeyHeld) {
-        player.blockHoldTimer += delta
-
-        if (player.blockHoldTimer >= HOLD_THRESHOLD) {
-            player.blocking = true
-            player.pendingBlockTap = false
-
-            // show glowing defensive/blocking sprite
-            player.graphics.use(Resources.defensiveState.toSprite())
+    if (player.blocking) {
+        player.blockTimer -= delta
+        if (player.blockTimer <= 0) {
+            player.blocking = false
+            player.blockTimer = 0
+            player.graphics.use(Resources.player2.toSprite())
         }
     }
-
-    if (blockKeyReleased) {
-        if (player.pendingBlockTap && player.blockHoldTimer < HOLD_THRESHOLD) {
-            cycleBlockType(player)
-        }
-
-        player.blocking = false
-        player.blockHoldTimer = 0
-        player.pendingBlockTap = false
-
-        // return to normal player2 sprite
-        player.graphics.use(Resources.player2.toSprite())
-    }
-}
-
-function cycleBlockType(player) {
-    player.blockType = (player.blockType % BLOCK_TYPES_COUNT) + 1
 }
 
 export function isBlocking(player) {
