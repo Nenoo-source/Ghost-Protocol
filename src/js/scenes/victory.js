73c7a650c7f2fb67d18b964bc5a8cbc6d0dbb11c
hoogendoornmax-code <hoogendoornmax@gmail.com>
2026-06-29 13:41:13 +0200

    vic

--- src/js/scenes/victory.js
+++ src/js/scenes/victory.js
@@ -13,6 +13,26 @@ export class Victory extends Scene {
         background.graphics.use(Resources.Victory.toSprite())
         this.add(background)
         background.scale = new Vector(0.85, 0.8)
+
+        // title
+        const title = new Actor({
+            pos: new Vector(engine.halfDrawWidth, engine.halfDrawHeight + 40),
+            anchor: new Vector(0.5, 0.8)
+        })
+
+        title.graphics.use(new Text({
+            text:
+                "Press space to play again",
+            font: new Font({
+                family: "Orbitron",
+                size: 30,
+                unit: FontUnit.Px,
+                color: Color.fromHex('#E0E0E0'),
+                strokeColor: Color.fromHex("#a8e1fb"),
+            })
+        }))
+
+        this.add(title)
     }
 
     onPreUpdate(engine) {
